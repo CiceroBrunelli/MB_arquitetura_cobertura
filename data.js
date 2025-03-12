1e56dc405e05db618ec04f390675ea19883e370d
CiceroBrunelli <cicerobrunelli@hotmail.com>
2025-03-12 17:07:01 -0300

    updtade project

--- data.js
+++ data.js
@@ -1,121 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-sute",
-      "name": "SUÍTE",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.7154534952551739,
-          "pitch": 0.1468767643278106,
-          "rotation": 0,
-          "target": "3-estar"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-banheiro",
-      "name": "BANHEIRO",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.0695498639710213,
-          "pitch": 0.06775513485381524,
-          "rotation": 0,
-          "target": "3-estar"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-hspedes",
-      "name": "HÓSPEDES",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.249168417154486,
-          "pitch": -0.01609935628652792,
-          "rotation": 0,
-          "target": "3-estar"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-estar",
+      "id": "0-estar",
       "name": "ESTAR",
       "levels": [
         {
@@ -144,34 +30,198 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.1243446194464326,
-          "pitch": -0.022879360016471395,
+          "yaw": -1.1306136819698391,
+          "pitch": 0.033363224728841345,
           "rotation": 0,
-          "target": "0-sute"
+          "target": "1-sute"
         },
         {
-          "yaw": -2.095938109114572,
-          "pitch": -0.03927316672112546,
+          "yaw": -0.9679552978336989,
+          "pitch": 0.012631541291254322,
           "rotation": 0,
-          "target": "2-hspedes"
+          "target": "4-banheiro"
         },
         {
-          "yaw": -0.9747947008505786,
-          "pitch": -0.03025048456651902,
+          "yaw": -1.3452118834323894,
+          "pitch": 0.03598701752611433,
           "rotation": 0,
-          "target": "1-banheiro"
+          "target": "3-hspedes"
         },
         {
-          "yaw": -2.872313975317592,
-          "pitch": -0.019280951138821223,
+          "yaw": -2.869851426253023,
+          "pitch": 0.03621517319070833,
           "rotation": 0,
-          "target": "4-gourmet-1"
+          "target": "5-gourmet-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-gourmet-1",
+      "id": "1-sute",
+      "name": "SUÍTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7462633409462072,
+          "pitch": 0.2328881571611845,
+          "rotation": 0,
+          "target": "0-estar"
+        },
+        {
+          "yaw": -0.1301072331163482,
+          "pitch": 0.032689195830782225,
+          "rotation": 0,
+          "target": "2-closet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-closet",
+      "name": "CLOSET",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0422867121864066,
+          "pitch": 0.13733213196372063,
+          "rotation": 0,
+          "target": "1-sute"
+        },
+        {
+          "yaw": -1.1601649310658964,
+          "pitch": -0.03472095728459479,
+          "rotation": 0,
+          "target": "4-banheiro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hspedes",
+      "name": "HÓSPEDES",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.468749740123979,
+          "pitch": 0.05292042073146064,
+          "rotation": 0,
+          "target": "0-estar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-banheiro",
+      "name": "BANHEIRO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0556986540111186,
+          "pitch": 0.02719702119837919,
+          "rotation": 0,
+          "target": "0-estar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-gourmet-1",
       "name": "GOURMET 1",
       "levels": [
         {
@@ -200,22 +250,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.32950290807696625,
-          "pitch": 0.16448840830610223,
+          "yaw": -0.3609593412676908,
+          "pitch": 0.09527471844582713,
           "rotation": 0,
-          "target": "5-gourmet-2"
-        },
-        {
-          "yaw": 2.9027130255153297,
-          "pitch": 0.28818004956328735,
-          "rotation": 0,
-          "target": "3-estar"
+          "target": "6-gourmet-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-gourmet-2",
+      "id": "6-gourmet-2",
       "name": "GOURMET 2",
       "levels": [
         {
@@ -244,10 +288,10 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.18347574116039134,
-          "pitch": 0.12402865452484768,
+          "yaw": 0.19511728771836623,
+          "pitch": 0.05223497810248823,
           "rotation": 0,
-          "target": "3-estar"
+          "target": "0-estar"
         }
       ],
       "infoHotspots": []
